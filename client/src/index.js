36ed55aec2cfc01855a13d3ab69714d5c28fc5f5
Marlapati Nitheesh <marlapati.2023ug1112@iiitranchi.ac.in>
2025-09-01 22:52:28 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { RoleProvider } from './context/RoleContext';
 
-export const BACKEND_URL = 'http://72.60.96.189/api';
+export const BACKEND_URL = 'http://72.60.96.189';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
